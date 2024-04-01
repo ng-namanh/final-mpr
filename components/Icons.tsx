d435b998e293749d0b928b43799d4119ed25c458
ng-namanh <namanh200313@gmail.com>
2024-04-02 03:11:08 +0700

    fix: type error on cssInterop, need to install @types/react-native

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,5 +1,5 @@
-import { Info, LucideIcon, MoonStar, Sun } from 'lucide-react-native';
-import { cssInterop } from 'nativewind';
+import { Info, LucideIcon, MoonStar, Sun } from 'lucide-react-native'
+import { cssInterop } from 'nativewind'
 
 function interopIcon(icon: LucideIcon) {
   cssInterop(icon, {
@@ -7,14 +7,14 @@ function interopIcon(icon: LucideIcon) {
       target: 'style',
       nativeStyleToProp: {
         color: true,
-        opacity: true,
-      },
-    },
-  });
+        opacity: true
+      }
+    }
+  })
 }
 
-interopIcon(Info);
-interopIcon(MoonStar);
-interopIcon(Sun);
+interopIcon(Info)
+interopIcon(MoonStar)
+interopIcon(Sun)
 
-export { Info, MoonStar, Sun };
+export { Info, MoonStar, Sun }
