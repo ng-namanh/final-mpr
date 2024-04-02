516dfb9aa0923e81f982a4c6dc8687b34b2435bc
ng-namanh <namanh200313@gmail.com>
2024-04-03 00:27:59 +0700

    chore: relocation useColorScheme from lib to hooks

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,19 +1,19 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Pressable, View } from 'react-native';
-import { MoonStar, Sun } from '~/components/Icons';
-import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
-import { useColorScheme } from '~/lib/useColorScheme';
-import { cn } from '~/lib/utils';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Pressable, View } from 'react-native'
+import { MoonStar, Sun } from '~/components/Icons'
+import { setAndroidNavigationBar } from '~/lib/android-navigation-bar'
+import { useColorScheme } from '~/hooks/useColorScheme'
+import { cn } from '~/lib/utils'
 
 export function ThemeToggle() {
-  const { isDarkColorScheme, setColorScheme } = useColorScheme();
+  const { isDarkColorScheme, setColorScheme } = useColorScheme()
   return (
     <Pressable
       onPress={() => {
-        const newTheme = isDarkColorScheme ? 'light' : 'dark';
-        setColorScheme(newTheme);
-        setAndroidNavigationBar(newTheme);
-        AsyncStorage.setItem('theme', newTheme);
+        const newTheme = isDarkColorScheme ? 'light' : 'dark'
+        setColorScheme(newTheme)
+        setAndroidNavigationBar(newTheme)
+        AsyncStorage.setItem('theme', newTheme)
       }}
       className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
     >
@@ -25,12 +25,16 @@ export function ThemeToggle() {
           )}
         >
           {isDarkColorScheme ? (
-            <MoonStar className='text-foreground' size={23} strokeWidth={1.25} />
+            <MoonStar
+              className='text-foreground'
+              size={23}
+              strokeWidth={1.25}
+            />
           ) : (
             <Sun className='text-foreground' size={24} strokeWidth={1.25} />
           )}
         </View>
       )}
     </Pressable>
-  );
+  )
 }
